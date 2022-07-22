9b53e5c514dc5a4887d4fced7b2c30d785649353
GunturuAdithya <adithyagunturu@gmail.com>
2022-07-22 19:06:55 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "aed6055416msh35e0a09110d47f2p1be161jsn1a7e9d28a683",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "371eedfbdbb86b3388b9c389189da489";
+
