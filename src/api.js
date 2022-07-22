a8bef9609cba4942e310f5d43fd35cd16a5adc52
GunturuAdithya <adithyagunturu@gmail.com>
2022-07-22 19:07:03 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "aed6055416msh35e0a09110d47f2p1be161jsn1a7e9d28a683",
+    "X-RapidAPI-Key": "API_KEY",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -9,5 +9,5 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "371eedfbdbb86b3388b9c389189da489";
+export const WEATHER_API_KEY = "API_KEY";
 
